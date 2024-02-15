7daf57a0cae48ebdc09b61847dcd701772b1bd1d
Leo <leo@mail.com>
2024-02-14 19:40:22 -0700

    14/02/24

---
+++ resources/views/blankpage.blade.php
@@ -0,0 +1,18 @@
+@extends('app')
+
+@section('titulo')
+    <h1>Consultar Reportes </h1>
+@stop
+
+@section('breadcrum')
+    <li class="breadcrumb-item"><a href="{{ url('/home') }}">Inicio</a></li>
+    <li class="breadcrumb-item active">Blank page</li>
+@stop
+
+@section('contenido')
+
+
+    <div class="custom-letter-spacing" style=""> wasamamaya </div>o
+
+
+@stop
