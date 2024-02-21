c38b2f1307bba1fdfba364b786b800753f2149d6
leo <leo@mail.com>
2024-02-20 19:46:52 -0700

    20/02/24

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,27 @@
 
 @section('home')
 
-    <h3 class="mb-2">Consultar Reportes</h3>
+<br>
+    <h3 class="mb-2">Consultar Reportes
+    <button type="button" class="btn btn-danger" style="float: right; height: 55px; width: 130px;">Nuevo Reporte</button></h3>
     <br><br><br>
-<div class="card card-secondary">
-                    <div class="card-body table-responsive wasamamaya">
-                        @yield('contenido')
-                        aahahaahahh
+                <!--<div class="card card-secondary">
+                <div class="card-body table-responsive wasamamaya">
+                    @yield('contenido')
                     </div>
-                </div>
+                </div> -->
+                <div class="table-responsive">
+            <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>nose</th>
+                    <th>ok</th>
+                    <th>si</th>
+                    <th>que</th>
+                </tr>
+            </thead>
+        </table>
     </div>
+
 @endsection
